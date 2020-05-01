f8f333d621ff5bf69ce50d52fcba543a40b196fa
Jonathan Alejandro González <38188085+tatanlejo17@users.noreply.github.com>
2020-05-01 12:05:09 -0500

    Construir métodos para consultar API usando fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatosAPIService } from './controllerServices/datos-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Starwars';
+  /**
+   * Método constructor
+   * => Recibe como parámetro el servicio DatosAPIService
+   * con el fin de usar sus funcionalidades
+   * @param apiStarwars 
+   */
+  constructor(private apiStarwars: DatosAPIService) { }
+  
+  /**
+   * Método para obtener el id del botón presionado en el aside.
+   * => Recibe como parámetro un event para saber en donde se disparo el event
+   *    y así obtener el Id de los botones
+   * @param e 
+   */
+  botonPresionado (e) {
+    let item;
+
+    /**
+     * Los botones están compuestos por tags <a> y <span>
+     * Con este condicional obtendremos el id sin importar
+     * cual de las dos etiquetas presionemos.
+     */
+    if(e.target.nodeName === "SPAN") {
+      item = e.target.id;
+    } else {
+      item = e.target.children[0].id;
+    }
+
+    /**
+     * Llamar función obtenerDatosCompletosXItem del servicio DatosAPIService
+     * => Recibe como parámetro el item con el id obtenido
+     */
+    this.apiStarwars.construirUrl(item);
+  }
+
 }
--- src/app/controllerServices/datos-api.service.ts
+++ src/app/controllerServices/datos-api.service.ts
@@ -6,4 +6,39 @@ import { Injectable } from '@angular/core';
 export class DatosAPIService {
 
   constructor() { }
+  /**
+   * Método para construir la url del recurso que se va a consumir
+   * luego llama al método del servicio obtenerDatosAPI()
+   * => Recibe como parámetro el id del botón 
+   * @param item 
+   */
+  construirUrl (item) {
+    console.log(item);
+    let url = `https://swapi.py4e.com/api/${item}/`;
+    /**
+     * Llamar método obtenerDatosAPI() propio del servicio
+     * => Recibe como parámetro la url construida
+     */
+    this.obtenerDatosAPI(url);
+  }
+
+  /**
+   * Función para hacer Get a la API, obtener una respuesta
+   * y los datos, se usa la funcionalidad fetch API interfaz propia 
+   * del lenguaje JS para trabajar con el estándar HTTPRequest
+   * => Recibe como parámetro la url que se va a consumir o consultar
+   * @param url 
+   */
+  obtenerDatosAPI (url) {
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
 }
