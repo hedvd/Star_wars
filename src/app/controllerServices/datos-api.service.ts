f893239b8b8565cbbfaf96e58c53b743bca60dfb
Jonathan Alejandro González <38188085+tatanlejo17@users.noreply.github.com>
2020-05-03 21:49:54 -0500

    Pintar datos en contenedor usando template literal

--- src/app/controllerServices/datos-api.service.ts
+++ src/app/controllerServices/datos-api.service.ts
@@ -19,8 +19,9 @@ export class DatosAPIService {
      * Llamar método obtenerDatosAPI() propio del servicio
      * => Recibe como parámetro la url construida
      */
-    this.obtenerDatosAPI(url);
+    this.obtenerDatosAPI(url, item);
   }
+
 
   /**
    * Función para hacer Get a la API, obtener una respuesta
@@ -29,7 +30,7 @@ export class DatosAPIService {
    * => Recibe como parámetro la url que se va a consumir o consultar
    * @param url 
    */
-  obtenerDatosAPI (url) {
+  obtenerDatosAPI (url, item) {
     fetch(url)
       .then((res) => {
         return res.json();
@@ -39,7 +40,7 @@ export class DatosAPIService {
          * Una vez obtenidos los datos llamar el metodo que se encargará 
          * de mostrar la información en cards dentro del contenedor principal 
          */
-        this.pintarCards(data);
+        this.pintarCards(data, item);
       })
       .catch((error) => {
         console.log(error);
@@ -48,7 +49,34 @@ export class DatosAPIService {
   /**
    * Metodo para pintar la información en el contenedor principal
    */
-  pintarCards(datos){
-    
+  pintarCards(datos, item){
+    const contenedor = document.getElementById('contenedorCards');
+    let templateHTML = ""
+
+    for (let i = 0; i < datos.results.length; i++) {
+      
+      // Insertar template que se va a mostrar
+      templateHTML += `
+        <div class="card mb-4" style="width: 15rem;">
+          <div class="card-body">
+            <h5 class="card-title border-bottom pb-1">${datos.results[i].name}</h5>
+            <p class="card-text">
+              <span>Género : ${datos.results[i].gender}.</span><br />
+              <span>Altura : ${datos.results[i].height} cm.</span><br />
+              <span>Peso : ${datos.results[i].mass} kg.</span><br />
+              <span>Color de ojos : ${datos.results[i].eye_color}.</span><br />
+              <span>Color de cabello : ${datos.results[i].hair_color}.</span>
+            </p>
+          </div>
+        </div>
+      `;
+    }
+
+    contenedor.style.background = "#dbdbdb";
+    contenedor.style.padding = ".8rem";
+    contenedor.style.borderRadius = ".8rem";
+    document.querySelector('#contenedorCards').innerHTML = templateHTML;
+    console.log(datos, contenedor);
   }
+
 }
